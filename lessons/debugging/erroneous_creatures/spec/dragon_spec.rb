15feae20dc98ea3815b0a25909855d3c4ca011ed
Jorja Fleming <aset284@gmail.com>
2023-03-24 13:53:58 -0600

    Complete dragon debugginh

--- lessons/debugging/erroneous_creatures/spec/dragon_spec.rb
+++ lessons/debugging/erroneous_creatures/spec/dragon_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require'./lib/dragon'
 
 RSpec.describe Dragon do
-  xit "has a name" do
+  it "has a name" do
     dragon = Dragon.new("Ramoth", :gold, "Lessa")
 
     expect(dragon.name).to eq("Ramoth")
@@ -14,37 +14,37 @@ RSpec.describe Dragon do
     expect(dragon.rider).to eq("Lessa")
   end
 
-  xit "has a color" do
+  it "has a color" do
     dragon = Dragon.new("Ramoth", :gold, "Lessa")
 
     expect(dragon.color).to eq(:gold)
   end
 
-  xit "can be a different dragon" do
+  it "can be a different dragon" do
     dragon = Dragon.new("Mnementh", :bronze, "F'lar")
 
     expect(dragon.name).to eq("Mnementh")
   end
 
-  xit "a different dragon can also have a different rider" do
+  it "a different dragon can also have a different rider" do
     dragon = Dragon.new("Mnementh", :bronze, "F'lar")
 
     expect(dragon.rider).to eq("F'lar")
   end
 
-  xit "a different dragon can be a different color" do
+  it "a different dragon can be a different color" do
     dragon = Dragon.new("Mnementh", :bronze, "F'lar")
 
     expect(dragon.color).to eq(:bronze)
   end
 
-  xit "is born hungry" do
+  it "is born hungry" do
     dragon = Dragon.new("Canth", :brown, "F'nor")
 
     expect(dragon.hungry?).to eq(true)
   end
 
-  xit "needs to eat alot to be satisfied" do
+  it "needs to eat alot to be satisfied" do
     dragon = Dragon.new("Canth", :brown, "F'nor")
 
     expect(dragon.hungry?).to eq(true)
@@ -56,7 +56,7 @@ RSpec.describe Dragon do
     expect(dragon.hungry?).to eq(false)
   end
 
-  xit "can breath fire" do
+  it "can breath fire" do
     dragon = Dragon.new("Canth", :brown, "F'nor")
 
     dragon.eat
@@ -67,7 +67,7 @@ RSpec.describe Dragon do
     expect(dragon.breathe_fire).to eq(true)
   end
 
-  xit "is hungry after breathing fire" do
+  it "is hungry after breathing fire" do
     dragon = Dragon.new("Canth", :brown, "F'nor")
 
     dragon.eat
@@ -78,7 +78,7 @@ RSpec.describe Dragon do
     expect(dragon.hungry?).to eq(true)
   end
 
-  xit "can not breathe fire when hungry" do
+  it "can not breathe fire when hungry" do
     dragon = Dragon.new("Canth", :brown, "F'nor")
 
     expect(dragon.hungry?).to eq(true)
