24e734ab6a4258c785b67aa131896717a062184b
Jorja Fleming <aset284@gmail.com>
2023-04-18 18:54:52 -0600

    Add some to dishes methods

--- practice_independent_challenges/week2/potluck/lib/dish.rb
+++ practice_independent_challenges/week2/potluck/lib/dish.rb
@@ -1,16 +1,9 @@
-require './lib/dish'
-require 'rspec'
+class Dish
+attr_reader :name,
+            :category
 
-RSpec.describe Dish do
-
-  it "exists" do
-    dish = Dish.new("x", :y)
-    expect(dish).to be_instance_of(Dish)
-  end
-
-  it "has attibutes" do 
-    dish = Dish.new("Couscous Salad", :appetizer)
-    expect(dish.name).to eq("Couscous Salad")
-    expect(dish.category).to eq(:appetizer)
+  def initialize(name, category)
+    @name = name
+    @category = category
   end
 end
--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -1,7 +1,16 @@
-class Dish
+require './lib/dish'
+require 'rspec'
 
-  def initialize(name, category)
-    @name = name
-    @category = category
+RSpec.describe Dish do
+
+  it "exists" do
+    dish = Dish.new("x", :y)
+    expect(dish).to be_instance_of(Dish)
+  end
+
+  it "has attibutes" do 
+    dish = Dish.new("Couscous Salad", :appetizer)
+    expect(dish.name).to eq("Couscous Salad")
+    expect(dish.category).to eq(:appetizer)
   end
 end
