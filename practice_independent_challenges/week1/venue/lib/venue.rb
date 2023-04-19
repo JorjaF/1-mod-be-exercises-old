75262d2574776bb4e927a8e07e0a5b416ff56c80
Jorja Fleming <aset284@gmail.com>
2023-04-18 18:13:26 -0600

    IDK if i commited but I have completed thru Iteration 4

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,31 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patrons)
+    @patrons << patrons
+  end
+
+  def yell_at_patrons
+    cap_name = patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    @patrons.size > @capacity
+  end
+
+  def kick_out
+    while self.over_capacity? == true
+      @patrons.pop
+    end
+  end
 end
